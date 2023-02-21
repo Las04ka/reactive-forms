32e3619419195ccba8b8d9c5915d8e0cc101230a
Las04ka <denkostyuk@gmail.com>
2023-02-21 23:06:46 +0200

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators
+} from "@angular/forms";
+import { delay, Observable, of, map } from "rxjs";
+
+enum Technologies {
+  Angular = 'angular',
+  React = 'react',
+  Vue = 'vue'
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'reactive-forms';
+  technologies = {
+    angular: ['1.1.1', '1.2.1', '1.3.3'],
+    react: ['2.1.2', '3.2.4', '4.3.1'],
+    vue: ['3.3.1', '5.2.1', '5.1.3']
+  }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthday: ['', Validators.required],
+      tech: [null, Validators.required],
+      techVersion: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email,], this.emailValidator],
+      hobbies: this.fb.array([
+        this.fb.group({
+          hobbyName: ['', Validators.required],
+          hobbyDuration: ['', Validators.required],
+        }),
+      ]),
+    })
+  }
+
+  emailValidator(control: FormControl,): Observable<ValidationErrors | null> {
+    return of(control.value).pipe(
+      delay(1000),
+      map((value) => (value === 'test@test.test' ? {forbiddenEmail: true} : null)),
+    );
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
+
+  onAddHobby():void {
+    this.hobbies.push(
+      this.fb.group({
+          hobbyName: ['', Validators.required],
+          hobbyDuration: ['', Validators.required]
+        }
+      )
+    )
+  }
+
+  onDeleteHobby(i: number):void {
+    this.hobbies.removeAt(i)
+  }
+
+  get selectedTechnology():Technologies {
+    return this.form.get('tech')?.value
+  }
+
+  get hobbies():FormArray {
+    return <FormArray>this.form.get('hobbies')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
